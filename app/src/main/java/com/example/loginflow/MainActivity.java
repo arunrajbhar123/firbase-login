43e756809ddc32c8d487ab06638a3573057e69ca
Arun Rajbhar <arunrajbhar@mayhem-studios.com>
2023-11-07 21:49:51 +0530

    main activity created button to navigate the activity

--- app/src/main/java/com/example/loginflow/MainActivity.java
+++ app/src/main/java/com/example/loginflow/MainActivity.java
@@ -4,17 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.loginflow.ui.Home;
 import com.example.loginflow.ui.Login;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginActivity, profile;
+
+    FirebaseAuth auth;
+    TextView loginStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, Login.class);
-        startActivity(intent);
+        loginActivity = findViewById(R.id.loginActivity);
+        profile = findViewById(R.id.profile);
+        loginStatus = findViewById(R.id.loginStatus);
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            loginStatus.setText(String.format("%s User is Login", auth.getCurrentUser().getDisplayName()));
+        } else {
+            loginStatus.setText("User is not Login");
+        }
+
+        loginActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity(Login.class);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity(Home.class);
+            }
+        });
+
 
     }
+
+    private void changeActivity(Class activityClass) {
+        Intent intent = new Intent(MainActivity.this, activityClass);
+        startActivity(intent);
+    }
+
+
 }
